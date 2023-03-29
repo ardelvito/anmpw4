b4efc824e70e95504362aa789b0331cea7afa198
ardelvito <ardelvito8738@gmail.com>
2023-03-29 13:08:10 +0700

    Exercise Week 4

--- app/src/main/java/com/example/anmp_w4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/example/anmp_w4/view/StudentDetailFragment.kt
@@ -1,11 +1,18 @@
 package com.example.anmp_w4.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.anmp_w4.R
+import com.example.anmp_w4.model.Student
+import com.example.anmp_w4.viewmodel.DetailViewModel
+import com.example.anmp_w4.viewmodel.ListViewModel
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 
 /**
@@ -15,12 +22,45 @@ import com.example.anmp_w4.R
  */
 class StudentDetailFragment : Fragment() {
 
+    private lateinit var viewModel: DetailViewModel
+    private val studentListAdapter = StudentListAdapter(arrayListOf())
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_student_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        observeStudentData()
+
+    }
+
+
+    private fun observeStudentData() {
+        viewModel.studentLD.observe(viewLifecycleOwner){
+            studentListAdapter.updateStudentList(it)
+            Log.d("tester", "test")
+            Log.d("testing data", it.toString())
+            Log.d("Name", it[0].name.toString())
+            val txtStudentName = view?.findViewById<TextInputEditText>(R.id.txtEditName)
+            val txtStudentId = view?.findViewById<TextInputEditText>(R.id.txtEditId)
+            val txtBirthDate = view?.findViewById<TextInputEditText>(R.id.txtEditBirth)
+            val txtPhone = view?.findViewById<TextInputEditText>(R.id.txtEditPhone)
+
+            txtStudentName?.setText(it[0].name.toString())
+            txtStudentId?.setText(it[0].id.toString())
+            txtBirthDate?.setText(it[0].dob.toString())
+            txtPhone?.setText(it[0].phone.toString())
+
+        }
     }
 
 }
--- app/src/main/java/com/example/anmp_w4/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/anmp_w4/viewmodel/DetailViewModel.kt
@@ -5,9 +5,12 @@ import androidx.lifecycle.ViewModel
 import com.example.anmp_w4.model.Student
 
 class DetailViewModel: ViewModel() {
-    val studentID = MutableLiveData<Student>()
+    val studentLD = MutableLiveData<ArrayList<Student>>()
 
     fun fetch(){
-        studentID.value = Student("16055","Nonie","1998/03/28","5718444778","http://dummyimage.com/75x100.jpg/cc0000/ffffff")
+        studentLD.value = arrayListOf(
+            Student("16055","Nonie","1998/03/28","5718444778","http://dummyimage.com/75x100.jpg/cc0000/ffffff")
+        )
+//        studentLD.value = Student("16055","Nonie","1998/03/28","5718444778","http://dummyimage.com/75x100.jpg/cc0000/ffffff")
     }
 }
