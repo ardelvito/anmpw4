0f28236e532d12eda10445e8684fca6f5a173d44
ardelvito <ardelvito8738@gmail.com>
2023-06-20 17:50:20 +0700

    Week 11 Data Binding - Fixed

--- app/src/main/java/com/example/anmp_w4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/example/anmp_w4/view/StudentDetailFragment.kt
@@ -8,8 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.anmp_w4.R
+import com.example.anmp_w4.databinding.FragmentStudentDetailBinding
+import com.example.anmp_w4.databinding.StudentListItemBinding
 import com.example.anmp_w4.model.Student
 import com.example.anmp_w4.util.loadImage
 import com.example.anmp_w4.viewmodel.DetailViewModel
@@ -25,71 +30,59 @@ import kotlinx.android.synthetic.main.student_list_item.*
 import java.util.concurrent.TimeUnit
 
 
-/**
- * A simple [Fragment] subclass.
- * Use the [StudentDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class StudentDetailFragment : Fragment() {
-
+class StudentDetailFragment : Fragment(), ButtonUploadClickListener  {
     private lateinit var viewModel: DetailViewModel
-    private val studentListAdapter = StudentListAdapter(arrayListOf())
+    private lateinit var dataBinding: FragmentStudentDetailBinding
 
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+        dataBinding = DataBindingUtil.inflate<FragmentStudentDetailBinding>(
+            inflater, R.layout.fragment_student_detail, container, false)
+
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
-//        viewModel.fetch()
-        val studentId = StudentDetailFragmentArgs.fromBundle(requireArguments()).studentId
-        viewModel.readDetails(studentId)
-        observeStudentData()
+        super.onViewCreated(view, savedInstanceState)
 
-    }
+//        val studentId = StudentDetailFragmentArgs.fromBundle(requireArguments()).studentId
+//
+//        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+//        viewModel.fetch(studentId)
+//
+//        observeViewModel(view)
 
+        if(arguments != null) {
+            val studentId = StudentDetailFragmentArgs.fromBundle(requireArguments()).studentId
 
-    private fun observeStudentData() {
-        viewModel.studentLD.observe(viewLifecycleOwner){
+            viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+            viewModel.readDetails(studentId)
 
+            observeViewModel()
 
-            val imgStudent = view?.findViewById<ImageView>(R.id.imageViewStudentDetail)
-            val txtStudentName = view?.findViewById<TextInputEditText>(R.id.txtEditName)
-            val txtStudentId = view?.findViewById<TextInputEditText>(R.id.txtEditId)
-            val txtBirthDate = view?.findViewById<TextInputEditText>(R.id.txtEditBirth)
-            val txtPhone = view?.findViewById<TextInputEditText>(R.id.txtEditPhone)
-            val btnNotif = view?.findViewById<Button>(R.id.btnNotif)
-
-
-//            imgStudent?.setImageResource(it[0].photoUrl)
-//            Picasso.get().load(it[0].photoUrl).into(imgStudent)
-            val student = it
-            btnNotif?.setOnClickListener{
-                Observable.timer(5, TimeUnit.SECONDS)
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe {
-                        Log.d("Messages", "five seconds")
-                        MainActivity.showNotif(student.name.toString(),
-                            "A new notification created",
-                            R.drawable.ic_baseline_circle_24)
-                    }
-
-            }
-
-            txtStudentName?.setText(it.name.toString())
-            txtStudentId?.setText(it.id.toString())
-            txtBirthDate?.setText(it.dob.toString())
-            txtPhone?.setText(it.phone.toString())
-            imgStudent?.loadImage(it.photoUrl, progressBarDetail)
-
+            dataBinding.listenerUpload = this
         }
     }
 
+    fun observeViewModel() {
+        viewModel.studentLD.observe(viewLifecycleOwner, Observer {
+            dataBinding.student = it
+        })
+    }
+
+    override fun onButtonUploadDetailClick(v: View) {
+        TODO("Not yet implemented")
+        Toast.makeText(v.context, "Update Success", Toast.LENGTH_SHORT).show()
+
+    }
 }
